b3eefcacd1b583a0e9e821c9d819c6139ec9bb11
James Acklin <james.acklin@gmail.com>
2018-05-01 12:56:24 -0400

    data-driven nav

--- components/Header.vue
+++ components/Header.vue
@@ -4,14 +4,26 @@
       <h1 class="white f6 f5-ns f4-l ma0 pa0 normal lh-copy">James Acklin</h1>
     </div>
     <ul class="fl list ma0 pa0 lh-copy w-50">
-      <li><nuxt-link class="nav-item link underline-hover white" to="/">Index</nuxt-link></li>
-      <li><nuxt-link class="nav-item link underline-hover white" to="/test1">Post 1</nuxt-link></li>
-      <li><nuxt-link class="nav-item link underline-hover white" to="/test2">Post 2</nuxt-link></li>
-      <li><nuxt-link class="nav-item link underline-hover white" to="/test3">Post 3</nuxt-link></li>
-      <li><nuxt-link class="nav-item link underline-hover white" to="/test4">Post 4</nuxt-link></li>
+      <li v-for="(page, key) in navigation" v-bind:key="key">
+        <nuxt-link class="nav-item link underline-hover white" v-bind:to="page[1]">{{ page[0] }}</nuxt-link>
+      </li>
     </ul>
   </nav>
 </template>
+
+<script>
+  export default {
+    data(){
+      return {
+        navigation: [
+          ['Index', '/'],
+          ['A page', '/test1'],
+          ['Another Page', '/test2'],
+        ]
+      }
+    }
+  }
+</script>
 
 <style scoped>
   nav { background: #9e9f89; }
