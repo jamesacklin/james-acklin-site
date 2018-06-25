59bf60a6780df3adff8a95c057a64c2b9863a0cf
James Acklin <james.acklin@gmail.com>
2018-06-25 14:57:38 -0400

    commit date-time on build, echo it in the UI. thanks to jon-kyle mohr

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="pa3 mw-9 lh-copy f6 bt b--silver cf">
     <div class="fl w-100 w-50-ns pr3">
       <p class="measure">All creative work property of its respective owner.</p>
-      <!-- <p class="measure">Updated somedate</p> -->
+      <p class="measure">Updated {{formattedDate}}</p>
     </div>
     <div class="fl w-100 w-50-ns">
       <h4 class="mb0 mt3 normal">Elsewhere</h4>
@@ -16,15 +16,41 @@
 </template>
 
 <script>
-  export default {
-    data(){
-      return {
-        social: {
-          'Email': 'mailto:james.acklin@gmail.com',
-          'Twitter': 'https://www.twitter.com/jamesacklin',
-          'Are.na': 'https://are.na/james-acklin/'
-        }
+var manifest = require('../manifest');
+export default {
+  data() {
+    return {
+      manifest,
+      social: {
+        Email: 'mailto:james.acklin@gmail.com',
+        Twitter: 'https://www.twitter.com/jamesacklin',
+        'Are.na': 'https://are.na/james-acklin/'
       }
+    };
+  },
+  computed: {
+    formattedDate: function() {
+      var date = new Date(this.manifest.updated);
+      var day = date.getDate();
+      var month = date.getMonth();
+      var year = date
+        .getFullYear()
+        .toString()
+        .substring(2);
+      var pad = function(n) {
+        return ('0' + n).slice(-2);
+      };
+      return (
+        [year, pad(month + 1), pad(day)].join('-') +
+        ' @ ' +
+        [
+          pad(date.getHours()),
+          pad(date.getMinutes()),
+          pad(date.getSeconds())
+        ].join(':') +
+        'UTC'
+      );
     }
   }
+};
 </script>
