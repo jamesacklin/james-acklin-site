0583d594a080f183bd6cfcf8e2bfdee355dad90a
James Acklin <james.acklin@gmail.com>
2019-01-13 22:25:48 -0500

    Reading list notice

--- pages/reading.vue
+++ pages/reading.vue
@@ -2,7 +2,7 @@
   <div class="content lh-copy center mw-9 pa3">
     <h1>Reading</h1>
     <p>
-      The most recent 5 items from my reading list.
+      The most recent 5 items from my reading list. May be slightly out of sync, since this file is statically generated from time to time.
       <small>
         <a href="https://www.are.na/james-acklin/reading-1527615453">
           ✶✶
