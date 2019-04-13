a4e708b8a27c7456c0b81a68ca65daed561a2777
James Acklin <james.acklin@gmail.com>
2019-04-13 10:38:20 -0400

    prettier + eslint working

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <div id="nav">
-      ["<router-link to="/">Home</router-link>",
-      "<router-link to="/about">About</router-link>"]
+      ["
+      <router-link to="/">Home</router-link>",
+      "
+      <router-link to="/about">About</router-link>"]
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,400i');
+@import url("https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,400i");
 
 :root {
   --bgcolor: 255, 255, 255;
@@ -18,7 +20,9 @@
   --mono: "IBM Plex Mono", menlo, monaco, monospace;
 }
 
-*, *:before, *:after {
+*,
+*:before,
+*:after {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
@@ -28,11 +32,11 @@ html {
   font-size: 16px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  -moz-font-feature-settings: "kern" 1; 
-  -ms-font-feature-settings: "kern" 1; 
-  -o-font-feature-settings: "kern" 1; 
-  -webkit-font-feature-settings: "kern" 1; 
-  font-feature-settings:"kern" 1;
+  -moz-font-feature-settings: "kern" 1;
+  -ms-font-feature-settings: "kern" 1;
+  -o-font-feature-settings: "kern" 1;
+  -webkit-font-feature-settings: "kern" 1;
+  font-feature-settings: "kern" 1;
   font-kerning: normal;
 }
 
@@ -44,10 +48,17 @@ body {
   font-family: var(--mono);
 }
 
-::-moz-selection { background: rgba(127, 127, 127, 0.5) }
-::selection { background: rgba(127, 127, 127, 0.5) }
+::-moz-selection {
+  background: rgba(127, 127, 127, 0.5);
+}
+::selection {
+  background: rgba(127, 127, 127, 0.5);
+}
 
-h1, h2, h3, h4 {
+h1,
+h2,
+h3,
+h4 {
   font-size: inherit;
   font-weight: normal;
 }
@@ -55,5 +66,4 @@ h1, h2, h3, h4 {
 a {
   color: inherit;
 }
-
 </style>
