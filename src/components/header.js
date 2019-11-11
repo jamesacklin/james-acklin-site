89f9ff64f719f61077557365ace2d50657d3c20c
James Acklin <james.acklin@gmail.com>
2019-11-11 12:42:46 -0500

    email link in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,14 @@
 import React from "react"
+import styled from "styled-components"
+
+
+const LinkBtn = styled.a.attrs({
+  className: `sans-serif ma0 lh-solid pa2 bg-black white ttu link fixed top-1 right-1 z-max hover-bg-white hover-black`
+})``
 
 const Header = () => (
   <header>
-    <div>
-    </div>
+    <LinkBtn href="mailto:james@ackl.in">E-mail</LinkBtn>
   </header>
 )
 
